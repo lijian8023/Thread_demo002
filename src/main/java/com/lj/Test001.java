d9976b14769d78560ef81e967a89a3faad9e4edb
yuanwang <yuanwang@DESKTOP-USV9V99>
2019-01-29 16:42:01 +0800

    thread 线程安全

---
+++ src/main/java/com/lj/Test001.java
@@ -0,0 +1,19 @@
+package com.lj;
+
+class ThreadDemo001 implements Runnable{
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+/**
+ * 模拟线程同步   共享火车票案例
+ * @author lj
+ *
+ */
+public class Test001 {
+
+}
